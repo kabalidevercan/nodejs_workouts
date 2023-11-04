20c96e9d4fd6a7f66b58c35740466e2ce4632ffe
kabali ercan <kabalidevercan@gmail.com>
2023-11-04 15:17:06 +0300

    1.0.1

--- nodejs_test_three/index.js
+++ nodejs_test_three/index.js
@@ -1,7 +1,61 @@
-const http = require('http')
-const server  = http.createServer((req,res) => {
-    res.writeHead(200, {
-        'Content-Type' : 'text/plain',
-    })
-    res.end('Hello Ercan is your friend')
+const express = require('express');
+
+
+const app = express()
+
+
+const PORT= 3000;
+
+const dudes = [
+    {
+        id:0 ,
+        name: 'Ercan John CECECE'
+    },
+    {
+        id:1 ,
+        name: 'John Kabaliw'
+    }
+]
+
+app.use((req,res,next)=> {
+    const start =  Date.now();
+    next();
+    const delta = Date.now() - start
+    console.log(`${req.method} and ${req.url} ${delta}`)
+})
+
+
+
+
+
+app.get('/dudes',(req,res) => {
+    res.status(200).json(dudes)
+})
+
+app.get('/dudes/:dudeId',(req,res)=> {
+    const dudeId = Number(req.params.dudeId);
+    const dude = dudes[dudeId]
+    if(dude) {
+        res.status(200).json(dude)
+    }else {
+        res.status(404).json({
+            error: 'Dude does not exist'
+        })
+    }}
+     )
+
+
+
+
+app.get('/messages', (req,res)=> {
+    res.send('<ul><li>Hello John,Again!</li></ul>')
+    
+})
+
+app.post('/messages',(req,res)=> {
+    console.log('Updating Messages...!');
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Listening on ${PORT}`)
 })
--- nodejs_test_two/index.js
+++ nodejs_test_two/index.js
@@ -1,25 +1,41 @@
 const parse = require('csv-parse')
-
 const fs = require('fs')
-
-
+// import {parse} from 'csv-parse'
+//  import { createReadStream } from 'node:fs'
 const results = []
+
+
 
 fs.createReadStream('kepler_data.csv')
 .pipe(parse({
-    comment: '#',
+    comment : '#',
     columns : true
 }))
-.on('data', (data) => {
-    results.push(data);
+.on('data', (data)=> {
+results.push(data)
 })
-.on('error',(err)=> {
+.on('error',(err)=>{
     console.log(err)
 })
-.on('end', ()=> {
+.on('end',()=> {
     console.log(results)
-    console.log('done')
+    console.log('end')
 })
+// fs.createReadStream('kepler_data.csv')
+// .pipe(parse({
+//     comment: '#',
+//     columns : true
+// }))
+// .on('data', (data) => {
+//     results.push(data);
+// })
+// .on('error',(err)=> {
+//     console.log(err)
+// })
+// .on('end', ()=> {
+//     console.log(results)
+//     console.log('done')
+// })
 
 
 
